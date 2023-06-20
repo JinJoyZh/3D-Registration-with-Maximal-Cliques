9c300ce9c837ff85600fee9b01e652c2ad789def
JerryZhang <jin.joy.zh@gmail.com>
2023-06-20 08:33:20 +0800

    recover deleted files

--- Linux/main.cpp
+++ Linux/main.cpp
@@ -17,14 +17,7 @@ bool low_inlieratio;
 bool no_logs;
 
 string threeDMatch[8] = {
-	"7-scenes-redkitchen",
-	"sun3d-home_at-home_at_scan1_2013_jan_1",
-	"sun3d-home_md-home_md_scan9_2012_sep_30",
-	"sun3d-hotel_uc-scan3",
-	"sun3d-hotel_umd-maryland_hotel1",
-	"sun3d-hotel_umd-maryland_hotel3",
-	"sun3d-mit_76_studyroom-76-1studyroom2",
-	"sun3d-mit_lab_hj-lab_hj_tea_nov_2_2012_scan1_erika",
+	"7-scenes-redkitchen"
 };
 
 string threeDlomatch[8] = {
@@ -341,7 +334,7 @@ int main(int argc, char** argv) {
 	}
 	else if (datasetName == "3dmatch")
 	{
-		for (size_t i = id; i < 8; i++)
+		for (size_t i = id; i < 1; i++)
 		{
 			cout << i + 1 << ":" << threeDMatch[i] << endl;
 			string analyse_csv = resultPath + "/" + threeDMatch[i] + "_" + descriptor + ".csv";
